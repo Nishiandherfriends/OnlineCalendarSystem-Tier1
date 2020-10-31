b5776ca449fc89e821826f38bf239aefbc8a5e4b
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2020-10-31 10:29:42 +0100

    Update Invite.cs

--- Models/Invite.cs
+++ Models/Invite.cs
@@ -1,17 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace OnlineCalendarSystem_Tier1.Models
 {
     public class Invite
     {
-        //Maybe I should take out the userInvitingID and put the userInvitingUsername
         public int userInvitingID { get; set; }
+        public string userInvitingName { get; set; }
         public int userInvitedID { get; set; }
         public int eventID { get; set; }
-        //Maybe add the event name and description
+        public string eventTitle { get; set; }
+        public string eventDescription { get; set; }
+        public Date startingDate { get; set; }
+        public Date endDate { get; set; }
 
         public Invite()
         {
