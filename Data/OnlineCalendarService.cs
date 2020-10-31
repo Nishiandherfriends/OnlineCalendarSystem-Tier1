d5c7e2663ae5d8bbe2b3ce3c76a81caa45014d00
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2020-10-31 10:15:29 +0100

    Deleted some files in the data folder

---
+++ Data/OnlineCalendarService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineCalendarSystem_Tier1.Data
+{
+    public class OnlineCalendarService
+    {
+    }
+}
