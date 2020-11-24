e93fa2e71ce04778dfb76c4f2b45f36f34bc08a0
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2020-11-24 11:44:37 +0100

    Now the updateUser method work PLEASE READ THE COMMENTS

--- Data/OnlineCalendarService.cs
+++ Data/OnlineCalendarService.cs
@@ -32,7 +32,7 @@ namespace OnlineCalendarSystem_Tier1.Data
         // The user class will always have a level two security exept for the admin which has already been created
         public async Task<string> createUser(string username, string password)
         {
-            using (HttpResponseMessage response = await ApiHelper.ApiClient.PutAsJsonAsync("/user?username=" + username + "&password=" + password, ""))
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PostAsJsonAsync("/user?username=" + username + "&password=" + password, ""))
             {
                 if (response.IsSuccessStatusCode)
                 {
@@ -45,7 +45,25 @@ namespace OnlineCalendarSystem_Tier1.Data
             }
         }
 
-        //public async Task<string> updateUser(User user) { return null; }
+        // This method will return a Task<string> that can be parsed into a string if the request is successful
+        // You just need to copy this " String string = await updateUser(username, password, id); "
+        // This method should only alter the user details from the user that's logged in
+        // You should put the details even if you don't want to change them
+        // The id can't change because it'll be used to find the user in the database
+        public async Task<string> updateUser(string username, string password, int id) 
+        {
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.PutAsJsonAsync("/user?username=" + username + "&password=" + password + "&id=" + id, ""))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return "Success";
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
 
 
         //public async Task<ArrayList<Event>> getEvents(int userID) { return null; }
