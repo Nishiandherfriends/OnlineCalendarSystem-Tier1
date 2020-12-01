4043a5771dea5f7047364ef6d244cb981d834268
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2020-12-01 14:03:36 +0100

    Update Invite.cs

--- Models/Invite.cs
+++ Models/Invite.cs
@@ -7,26 +7,21 @@ using System.Threading.Tasks;
 namespace OnlineCalendarSystem_Tier1.Models
 {
     /// <summary>
-    /// This class hold the Invite information such as the username of the user that is inviting, the id of the user invited,
-    /// the id of the event, the starting date and the end date.
+    /// This class hold the Invite information such as the username of the user that is inviting, the id of the user invited and the event to which the user is invited.
     /// </summary>
     public class Invite
     {
         public string userInvitingUsername { get; set; }
         public int userInvitedID { get; set; }
-        public int eventID { get; set; }
-        public Date startingDate { get; set; }
-        public Date endDate { get; set; }
+        public Event evt { get; set; }
 
         public Invite() {}
 
-        public Invite(string userInvitingUsername, int userInvitedID, int eventID, Date startingDate, Date endDate)
+        public Invite(string userInvitingUsername, int userInvitedID, Event evt)
         {
             this.userInvitingUsername = userInvitingUsername;
             this.userInvitedID = userInvitedID;
-            this.eventID = eventID;
-            this.startingDate = startingDate;
-            this.endDate = endDate;
+            this.evt = evt;
         }
     }
 }
