b6f90f7d325295c8d051343b2147da7503e3fb36
Javier Candeira <56227361+293177@users.noreply.github.com>
2020-12-08 11:52:36 +0100

    more work

--- obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,77 +13,77 @@ namespace OnlineCalendarSystem_Tier1.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 1 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 2 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 3 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 4 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 5 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 6 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 7 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 8 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using OnlineCalendarSystem_Tier1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 9 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using OnlineCalendarSystem_Tier1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\Pages\Index.razor"
+#line 2 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\Pages\Index.razor"
 using OnlineCalendarSystem_Tier1.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\Pages\Index.razor"
+#line 3 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\Pages\Index.razor"
 using OnlineCalendarSystem_Tier1.Data;
 
 #line default
@@ -98,7 +98,7 @@ using OnlineCalendarSystem_Tier1.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 172 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\Pages\Index.razor"
+#line 172 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\Pages\Index.razor"
       
 
     private Date selectedDay = new Date
--- obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
+++ obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/SurveyPrompt.razor.g.cs
@@ -13,77 +13,77 @@ namespace OnlineCalendarSystem_Tier1.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 1 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 2 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 3 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 4 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 5 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 6 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 7 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 8 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using OnlineCalendarSystem_Tier1;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 9 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using OnlineCalendarSystem_Tier1.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 10 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using OnlineCalendarSystem_Tier1.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\_Imports.razor"
+#line 11 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\_Imports.razor"
 using OnlineCalendarSystem_Tier1.Data;
 
 #line default
@@ -97,7 +97,7 @@ using OnlineCalendarSystem_Tier1.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 12 "C:\Users\javic\RiderProjects\OnlineCalendarSystem-Tier1\Shared\SurveyPrompt.razor"
+#line 12 "c:\Users\javic\source\repos\OnlineCalendarSystem-Tier1\OnlineCalendarSystem-Tier1\Shared\SurveyPrompt.razor"
        
     // Demonstrates how a parent component can supply parameters
     [Parameter]
